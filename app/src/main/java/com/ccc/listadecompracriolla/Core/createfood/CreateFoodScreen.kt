1da70d6b123fedec7097463626b7a6843339667d
Pc-Traki <CoalChillChicken@gmail.com>
2025-09-12 13:23:23 -0400

    arreglado bug de actualize product al volver

--- app/src/main/java/com/ccc/listadecompracriolla/Core/createfood/CreateFoodScreen.kt
+++ app/src/main/java/com/ccc/listadecompracriolla/Core/createfood/CreateFoodScreen.kt
@@ -7,6 +7,7 @@ package com.ccc.listadecompracriolla.Core.createfood
 arreglar error con el .
  */
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -101,6 +102,11 @@ fun CreateFoodScreen(
 
 
     LaunchedEffect(Unit) { focusElements.requestFocus() }
+
+    BackHandler(enabled = true) {
+        viewModel.actualizeProduct(-1)
+        navigateToback()
+    }
 
     Scaffold(
 //------------------------------------------topbar-----------------------------------------
