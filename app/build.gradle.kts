b53c6ae82039ae1b507bdd7e0f95ebf8735badc9
Pc-Traki <CoalChillChicken@gmail.com>
2025-10-01 19:31:14 -0400

    cambiado numero de version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         minSdk = 24
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
