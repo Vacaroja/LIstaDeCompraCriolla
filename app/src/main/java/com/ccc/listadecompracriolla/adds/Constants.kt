ab51c6e62707c4309e937f0ce0180cec7b532eee
Vacaroja <vr.alfonzo1@gmail.com>
2025-10-28 19:05:36 -0400

    ARREGLADO interstitial(COLOCADO CONSTANTE)

--- app/src/main/java/com/ccc/listadecompracriolla/adds/Constants.kt
+++ app/src/main/java/com/ccc/listadecompracriolla/adds/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
 
     const val AD_ID_BANNER = "ca-app-pub-4362864353957890/3980245948"
 
-    const val AD_ID_FULLSCREEN = "ca-app-pub-3940256099942544/1033173712"
+    const val AD_ID_FULLSCREEN = "ca-app-pub-4362864353957890/8955938080"
 }
