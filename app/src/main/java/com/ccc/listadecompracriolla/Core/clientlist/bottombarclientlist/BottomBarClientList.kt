494900fba638927f0e71da8ebf138821912cff37
Vacaroja <vr.alfonzo1@gmail.com>
2025-11-13 01:33:17 -0400

    colocado icono para porcentaje para las cuotas

--- app/src/main/java/com/ccc/listadecompracriolla/Core/clientlist/bottombarclientlist/BottomBarClientList.kt
+++ app/src/main/java/com/ccc/listadecompracriolla/Core/clientlist/bottombarclientlist/BottomBarClientList.kt
@@ -1,5 +1,7 @@
 package com.ccc.listadecompracriolla.Core.clientlist.bottombarclientlist
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -7,12 +9,17 @@ import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -20,10 +27,11 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ccc.listadecompracriolla.Core.clases.ProductViewModel
+import com.ccc.listadecompracriolla.Core.clientlist.bottombarclientlist.coutesview.CoutesModal
 import com.ccc.listadecompracriolla.Core.formatterdata.formatNumber
 import com.ccc.listadecompracriolla.R
-import com.ccc.listadecompracriolla.ui.theme.white
 import com.ccc.listadecompracriolla.ui.theme.purpleHeart
+import com.ccc.listadecompracriolla.ui.theme.white
 
 @Composable
 fun BottomClientList(modifier: Modifier = Modifier, viewModel: ProductViewModel) {
@@ -32,6 +40,7 @@ fun BottomClientList(modifier: Modifier = Modifier, viewModel: ProductViewModel)
     val total by viewModel.total.collectAsState()
     val inCar by viewModel.inCar.collectAsState()
     val tasa by viewModel.tasa.collectAsState()
+    var pressedPercent by remember { mutableStateOf(false) }
 
 
 //------------------------------------------Variables-----------------------------------------
@@ -67,12 +76,23 @@ fun BottomClientList(modifier: Modifier = Modifier, viewModel: ProductViewModel)
 
         }
 
-        VerticalDivider(
-            color = white, thickness = 2.dp,
-            modifier = modifier.padding(
-                vertical = 7.dp, horizontal = 20.dp
+        Box(contentAlignment = Alignment.Center){
+            VerticalDivider(
+                color = white, thickness = 2.dp,
+                modifier = modifier.padding(
+                    vertical = 7.dp, horizontal = 20.dp
+                )
             )
-        )
+            Card(border = BorderStroke(1.dp, white)) {
+                IconButton(onClick = { pressedPercent = true }) {
+                    Icon(
+                        painter = painterResource(R.drawable.percent_icon),
+                        contentDescription = "Cuotas",
+                    )
+                }
+            }
+
+        }
 //------------------------------------------BottomTotalPrice-----------------------------------------
 
         Icon(
@@ -99,6 +119,9 @@ fun BottomClientList(modifier: Modifier = Modifier, viewModel: ProductViewModel)
                 fontWeight = FontWeight.Bold,
                 color = white
             )
+        }
+        if (pressedPercent){
+            CoutesModal(total = total){pressedPercent = false }
         }
     }
 }
